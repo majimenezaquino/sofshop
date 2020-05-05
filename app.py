84208922ef02692807866b9e22362ca38468edea
majimenezaquino <your_email_address@example.com>
2020-05-05 03:30:11 +0200

    [ADD]: webhook

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, json, jsonify, request
 app=Flask(__name__)
 
 @app.route("/")
 def home():
     return "welcome flask"
 
+@app.route("/webhook",methods=["POST"])
+def get_callback():
+    print(request.get_json())
+    return "hola"
 
-if __name__ =="__main__":
-    app.run()
+
+
+if(__name__ =="__main__"):
+    app.run(debug=True)
